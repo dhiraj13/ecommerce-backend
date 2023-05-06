49375b17eb09bcc592b262e3deffc1566972f783
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-05-06 19:16:57 +0545

    add fields for get all product ctrl

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -69,6 +69,14 @@ const getAllProduct = asyncHandler(async (req, res) => {
       query = query.sort("-createdAt");
     }
 
+    // Limiting the Fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
     const product = await query;
     res.json(product);
   } catch (error) {
