0ab7e24833520a5338e0c805c924415e32d942f6
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-05-07 17:30:35 +0545

    add get product category ctrl

--- routes/prodCategoryRoute.js
+++ routes/prodCategoryRoute.js
@@ -3,6 +3,7 @@ const {
   createCategory,
   updateCategory,
   deleteCategory,
+  getCategory,
 } = require("../controller/prodCategoryCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/", authMiddleware, isAdmin, createCategory);
 router.put("/:id", authMiddleware, isAdmin, updateCategory);
 router.delete("/:id", authMiddleware, isAdmin, deleteCategory);
+router.get("/:id", getCategory);
 
 module.exports = router;
