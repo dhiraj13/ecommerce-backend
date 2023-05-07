fd87f806ff43091ed1faf72aa3a7c97123a8a72e
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-05-07 17:25:38 +0545

    add create and update  product category ctrl

---
+++ controller/prodCategoryCtrl.js
@@ -0,0 +1,26 @@
+const Category = require("../models/prodCategoryModel");
+const asyncHandler = require("express-async-handler");
+const validateMongoDbId = require("../utils/validateMongodbId");
+
+const createCategory = asyncHandler(async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.json(newCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createCategory, updateCategory };
---
+++ routes/prodCategoryRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const {
+  createCategory,
+  updateCategory,
+} = require("../controller/prodCategoryCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.post("/", authMiddleware, isAdmin, createCategory);
+router.put("/:id", authMiddleware, isAdmin, updateCategory);
+
+module.exports = router;
