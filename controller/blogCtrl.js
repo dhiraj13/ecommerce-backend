75ad42c40882c8a21aa3cd0a70edc8b14b2fd87a
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-05-07 12:13:39 +0545

    add update and get blog ctrl

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -24,4 +24,21 @@ const updateBlog = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createBlog, updateBlog };
+const getBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const getBlog = await Blog.findById(id);
+    await Blog.findByIdAndUpdate(
+      id,
+      {
+        $inc: { numViews: 1 },
+      },
+      { new: true }
+    );
+    res.json(getBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createBlog, updateBlog, getBlog };
