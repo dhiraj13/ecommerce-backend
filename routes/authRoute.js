a19360be2ec9d3b9b2dc68d9e073c1a1051ca341
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-06-08 17:16:35 +0545

    add get and create order and also udpate order status on user ctrl

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -20,6 +20,9 @@ const {
   getUserCart,
   emptyCart,
   applyCoupon,
+  createOrder,
+  getOrders,
+  updateOrderStatus,
 } = require("../controller/userCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -29,13 +32,21 @@ router.post("/login", loginUser);
 router.post("/admin-login", loginAdmin);
 router.post("/cart", authMiddleware, userCart);
 router.post("/cart/applycoupon", authMiddleware, applyCoupon);
+router.post("/cart/cash-order", authMiddleware, createOrder);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logoutUser);
 router.put("/edit-user", authMiddleware, updateUser);
 router.put("/save-address", authMiddleware, saveAddress);
 router.post("/forgot-password-token", forgotPasswordToken);
 router.put("/reset-password/:token", resetPassword);
+router.put(
+  "/order/update-order/:id",
+  authMiddleware,
+  isAdmin,
+  updateOrderStatus
+);
 router.get("/all-users", getAllUser);
+router.get("/get-orders", authMiddleware, getOrders);
 router.get("/wishlist", authMiddleware, getWishlist);
 router.get("/cart", authMiddleware, getUserCart);
 router.get("/:id", authMiddleware, isAdmin, getUser);
