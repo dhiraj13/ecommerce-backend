68aa61a9998ecc10d4938cf4a54b5985b7fdea5f
dhiraj khapangi <dkhapangi13@outlook.com>
2024-03-25 15:11:40 +0545

    add cors options

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
-const express = require("express");
-const dbConnect = require("./config/dbConnect");
-const app = express();
-const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 4000;
-const authRouter = require("./routes/authRoute");
-const productRouter = require("./routes/productRoute");
-const blogRouter = require("./routes/blogRoute");
-const prodCategoryRouter = require("./routes/prodCategoryRoute");
-const blogCategoryRouter = require("./routes/blogCategoryRoute");
-const brandRouter = require("./routes/brandRoute");
-const colorRouter = require("./routes/colorRoute");
-const enqRouter = require("./routes/enqRoute");
-const couponRouter = require("./routes/couponRoute");
-const uploadRouter = require("./routes/uploadRoute");
-const bodyParser = require("body-parser");
-const { notFound, errorHandler } = require("./middlewares/errorHandler");
-const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require("express")
+const dbConnect = require("./config/dbConnect")
+const app = express()
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT || 4000
+const authRouter = require("./routes/authRoute")
+const productRouter = require("./routes/productRoute")
+const blogRouter = require("./routes/blogRoute")
+const prodCategoryRouter = require("./routes/prodCategoryRoute")
+const blogCategoryRouter = require("./routes/blogCategoryRoute")
+const brandRouter = require("./routes/brandRoute")
+const colorRouter = require("./routes/colorRoute")
+const enqRouter = require("./routes/enqRoute")
+const couponRouter = require("./routes/couponRoute")
+const uploadRouter = require("./routes/uploadRoute")
+const bodyParser = require("body-parser")
+const { notFound, errorHandler } = require("./middlewares/errorHandler")
+const cookieParser = require("cookie-parser")
+const morgan = require("morgan")
+const cors = require("cors")
 
-dbConnect();
-app.use(morgan("dev"));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+const corsOptions = {
+  credentials: true,
+  origin: [
+    "http://localhost:5000",
+    "https://msrd-ecommerce-backend.vercel.app",
+  ],
+}
 
-app.use("/api/user", authRouter);
-app.use("/api/product", productRouter);
-app.use("/api/blog", blogRouter);
-app.use("/api/product-category", prodCategoryRouter);
-app.use("/api/blog-category", blogCategoryRouter);
-app.use("/api/brand", brandRouter);
-app.use("/api/color", colorRouter);
-app.use("/api/coupon", couponRouter);
-app.use("/api/enquiry", enqRouter);
-app.use("/api/upload", uploadRouter);
+dbConnect()
+app.use(morgan("dev"))
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use(notFound);
-app.use(errorHandler);
+app.use("/api/user", authRouter)
+app.use("/api/product", productRouter)
+app.use("/api/blog", blogRouter)
+app.use("/api/product-category", prodCategoryRouter)
+app.use("/api/blog-category", blogCategoryRouter)
+app.use("/api/brand", brandRouter)
+app.use("/api/color", colorRouter)
+app.use("/api/coupon", couponRouter)
+app.use("/api/enquiry", enqRouter)
+app.use("/api/upload", uploadRouter)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-  console.log(`Server is running at PORT ${PORT}`);
-});
+  console.log(`Server is running at PORT ${PORT}`)
+})
